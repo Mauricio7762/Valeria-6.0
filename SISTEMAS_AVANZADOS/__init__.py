41130c2a0b89fc601a81081ad1a1e99a0c7c2ea6
Mauricio7762 <70825035+Mauricio7762@users.noreply.github.com>
2026-08-16 04:26:43 -0300

    Add files via upload

---
+++ SISTEMAS_AVANZADOS/__init__.py
@@ -0,0 +1,19 @@
+"""Sistemas avanzados: metacognición, curiosidad, neurogénesis."""
+
+from .METACOGNICION import (
+    AjustadorMetacognitivo,
+    AutorreflexionMetacognitiva,
+    EvaluadorMetacognitivo,
+    MonitorMetacognitivo,
+    PlanificadorMetacognitivo,
+)
+from .CURIOSIDAD_COMPUTACIONAL import ExploradorAutonomo
+
+__all__ = [
+    "MonitorMetacognitivo",
+    "EvaluadorMetacognitivo",
+    "PlanificadorMetacognitivo",
+    "AjustadorMetacognitivo",
+    "AutorreflexionMetacognitiva",
+    "ExploradorAutonomo",
+]
